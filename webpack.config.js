63e04fefe10facacc1814d9050e70a14c38ae073
Simon Yu <simon94hyu@gmail.com>
2021-01-30 18:26:03 +1100

    Initialized by webpack, Ready for start

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, './src/build'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss?$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.jsx', '.js', '.tsx', '.ts'],
+  },
+  plugins: [new HtmlWebPackPlugin({ template: './src/index.html' })],
+};
